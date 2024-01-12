a4c179d88c0c30c2bce6a4f25bb154166185456d
GuiTrintinalia <156430860+GuiTrintinalia@users.noreply.github.com>
2024-01-12 11:05:21 -0300

    Update portfolio_optimizer.py

--- portfolio_optimizer.py
+++ portfolio_optimizer.py
@@ -74,9 +74,74 @@ tipo_dados = st.sidebar.button('Tipo de dados', ['info','history','actions'])
 
 # Get Tickers from yahoo finance
 msft = yf.Ticker("MSFT")
-hist = msft.history(period="1mo")
-actions = msft.actions
 
-st.dataframe(hist)
-st.dataframe(actions)
+
+
+# Displaying Dividends
+st.subheader("Dividends")
+st.dataframe(msft.dividends)
+
+# Displaying Splits
+st.subheader("Splits")
+st.dataframe(msft.splits)
+
+# Displaying Capital Gains
+st.subheader("Capital Gains")
+# Checking if the method exists for the selected stock
+if hasattr(msft, 'capital_gains'):
+    st.dataframe(msft.capital_gains)
+else:
+    st.write("Capital Gains data not available for this stock.")
+
+# Displaying Share Count
+st.subheader("Share Count")
+st.dataframe(msft.get_shares_full(start="2022-01-01", end=None))
+
+# Displaying Financials
+st.subheader("Income Statement")
+st.dataframe(msft.income_stmt)
+
+st.subheader("Quarterly Income Statement")
+st.dataframe(msft.quarterly_income_stmt)
+
+st.subheader("Balance Sheet")
+st.dataframe(msft.balance_sheet)
+
+st.subheader("Quarterly Balance Sheet")
+st.dataframe(msft.quarterly_balance_sheet)
+
+st.subheader("Cash Flow Statement")
+st.dataframe(msft.cashflow)
+
+st.subheader("Quarterly Cash Flow Statement")
+st.dataframe(msft.quarterly_cashflow)
+
+# Displaying Holders
+st.subheader("Major Holders")
+st.dataframe(msft.major_holders)
+
+st.subheader("Institutional Holders")
+st.dataframe(msft.institutional_holders)
+
+st.subheader("Mutual Fund Holders")
+st.dataframe(msft.mutualfund_holders)
+
+st.subheader("Insider Transactions")
+st.dataframe(msft.insider_transactions)
+
+st.subheader("Insider Purchases")
+st.dataframe(msft.insider_purchases)
+
+st.subheader("Insider Roster Holders")
+st.dataframe(msft.insider_roster_holders)
+
+# Displaying Recommendations
+st.subheader("Recommendations")
+st.dataframe(msft.recommendations)
+
+st.subheader("Recommendations Summary")
+st.dataframe(msft.recommendations_summary)
+
+st.subheader("Upgrades/Downgrades")
+st.dataframe(msft.upgrades_downgrades)
 
