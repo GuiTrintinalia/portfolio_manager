b434c61a7f1cf5ae944d64b5dae52cb4820f802e
GuiTrintinalia <156430860+GuiTrintinalia@users.noreply.github.com>
2024-01-12 09:38:38 -0300

    Update portfolio_optimizer.py

--- portfolio_optimizer.py
+++ portfolio_optimizer.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt 
+import plotly.express as px
+import requests
+import json
+import time
+import pickle
+import datetime
+from datetime import datetime, timedelta, date
+from itertools import combinations
+from multiprocessing import Pool
+import time
+import riskfolio as rp
+import yfinance  as yf
 
