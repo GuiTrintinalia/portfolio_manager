e1db4255e614f5272cab90defdbd0e89700906dc
GuiTrintinalia <156430860+GuiTrintinalia@users.noreply.github.com>
2024-01-17 19:52:40 -0300

    Update portfolio_optimizer.py

--- portfolio_optimizer.py
+++ portfolio_optimizer.py
@@ -64,7 +64,7 @@ def download_data(data, period):
         for name, ticker in data.items():
             ticker_obj = yf.Ticker(ticker)
             hist = ticker_obj.history(period=period)
-            hist.columns = [f"{ticker}_{col}" for col in hist.columns]  # Add prefix to the name
+            hist.columns = [f"{name}_{col}" for col in hist.columns]  # Add prefix to the name
             dfs.append(hist)
     elif isinstance(data, list):
         # If input is a list, assume tickers directly without names
