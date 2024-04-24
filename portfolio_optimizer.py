1375a7bad0fc1fcc3369a3eee05fbc500d384977
Guilherme Trintinalia <156430860+GuiTrintinalia@users.noreply.github.com>
2024-04-24 13:53:15 -0300

    Update portfolio_optimizer.py
    Signed-off-by: Guilherme Trintinalia <156430860+GuiTrintinalia@users.noreply.github.com>

--- portfolio_optimizer.py
+++ portfolio_optimizer.py
@@ -899,7 +899,7 @@ if session_state.data is not None:
             total_shares.append(weights_df['Weights'])
             session_state.df = compute_investments(session_state.data, tickers, total_shares, invested_cash)
         else:
-            st.markdown(f'Missing Allocation: {round(1- np.sum(weights_df["Weights"]),3}')
+            st.markdown(f'Missing Allocation: {round(1- np.sum(weights_df["Weights"]),3)}')
 	
 else:
     try:
