dadfcdbf7b563cb0256e68b7de67f4aea9601e82
Guilherme Trintinalia <156430860+GuiTrintinalia@users.noreply.github.com>
2024-04-24 10:08:34 -0300

    Update portfolio_optimizer.py
    Signed-off-by: Guilherme Trintinalia <156430860+GuiTrintinalia@users.noreply.github.com>

--- portfolio_optimizer.py
+++ portfolio_optimizer.py
@@ -888,7 +888,8 @@ if session_state.data is not None:
         if 'tickers' in globals() and tickers is not None:
             load_weights = st.button('Load weights')
             if load_weights:
-                fill_weights = pd.DataFrame({'Ticker': tickers, 'Weights': [''] * len(tickers)})
+                fill_weights = pd.DataFrame({'Ticker': tickers, 'Weights': np.nan})
+		st.dataframe(fill_weights)
                 weights_df = st.experimental_data_editor(fill_weights)
                 editions = weights_df.loc[weights_df['Weights'].idxmax()]['Ticker']
             else:
